df46c5da358f96a6af6e72b5c2420e4d1e291390
danielquintao <danielquintaodemoraes@gmail.com>
2022-11-21 20:04:50 -0300

    some advance in creatng the graph...

--- draft.scala
+++ draft.scala
@@ -4,32 +4,17 @@
 // For the moment, create a fake graph with fake features and labels
 // =============================================================================
 
-import org.apache.spark.graphx.{Graph, VertexId}
+import org.apache.spark.graphx.{Graph, VertexId, PartitionStrategy}
 import org.apache.spark.graphx.util.GraphGenerators
 import scala.util.Random
 import breeze.linalg._
 import breeze.stats.distributions.Gaussian
 import scala.math.{log, exp, sqrt}
 
-class LearningData(var features: DenseVector[Double], var label: Double)
 val rand = scala.util.Random
 val nIn = 2+1  // number of features INCLUDING BIAS
 val nHid = 3  // number of neurons in hidden layer
 val nOut = 1  // dimension of label
-
-// graph of train samples, annotated with features and labels
-val graph: Graph[LearningData, Int] =
-  GraphGenerators.logNormalGraph(sc, numVertices = 100).mapVertices(
-    (_, _) => {
-      var data = new
-        LearningData(
-          DenseVector.rand(nIn),
-          randomInt()  // 0 or 1
-        )
-      data.features(-1) = 1  // bias 
-      data
-    }
-  )
 
 // =============================================================================
 // 2- train a "common" NN before doing any GNN
@@ -118,3 +103,64 @@ val yt = DenseMatrix.ones[Double](5, nOut)  // fake true labels
 yt(DenseMatrix.rand(5, nOut) <:< 0.5) := 0.0
 nn.forward(x, true)
 nn.backward(yt, true) // only works after the forward
+
+// =============================================================================
+// 3- Generation of our super graph with data nodes and parameter nodes
+// =============================================================================
+
+// some definitions //? do we need it?
+class ParentVertexProperty()
+case class DataVertexProperty(
+  val data: LearningData // or val data: (DenseVector[Double], Double)
+) extends ParentVertexProperty
+case class ParamVertexProperty(
+  val w1: DenseMatrix[Double],
+  val w2: DenseMatrix[Double],
+  // val lr: Double, //? do we need it?
+  var nn: NN
+)  extends ParentVertexProperty
+
+
+// -----------------------------------------------------------------------------
+// 3.1 create our single graph with both Data and Parameter vertices
+// -----------------------------------------------------------------------------
+// 3.1.1 - Create data vertices, annotated with features and labels
+class LearningData(var features: DenseVector[Double], var label: Double)
+val dataGraph: Graph[LearningData, Int] =
+  GraphGenerators.logNormalGraph(sc, numVertices = 100).mapVertices( // obs: may have "recursive edges""
+    (_, _) => {
+      var data = new
+        LearningData(
+          DenseVector.rand(nIn),
+          randomInt()  // 0 or 1
+        )
+      data.features(-1) = 1  // bias 
+      data
+    }
+  )
+// 3.1.2 - Make the dataGraph undirected (in GraphX, this means duplicating+reversing all edges)
+var graph = Graph.apply(
+  vertices=dataGraph.vertices,
+  edges=dataGraph.edges.union(dataGraph.edges.reverse)
+)
+// Merge edges pointing in the same direction, specially if we already received some pairs A-B,B-A:
+//? (are we supposed to do this "deduplication" step or is the dataset a multigraph?)
+graph = graph.partitionBy(PartitionStrategy.CanonicalRandomVertexCut) // NOTE that groupEdges requires
+//                                                    a previous partitioning step to co-localize edges
+graph = graph.groupEdges{case (ed1, ed2) => ed1} // edges have no relevant annotation in our scenario,
+//                                                  so we simply take the first one 
+
+
+// 3.1.3 - Add parameter vertices where each has "batch-size" inward data->parameter edges.
+// A further improvement on the Routing Table (where GraphX says, for each vertex, in which partitions
+// we find its edges) is to use a constraint on the IDs of those data vertices that point toward the
+// the parameter vertex, e.g. that they are all congruent mod "nb partitions".
+val nPartitions = graph.edges.getNumPartitions
+val batchSize = 4 //TODO change to 512 with big dataset
+// TODO <-- PAREI AQUI
+
+// 3.1.4 - Partition the graph so that all edges point to the same node are together
+// (partition by dst vertex id)
+
+//! ATTENTION it seems that GraphSage was made for undirected graphs, so we need to replicate all
+//! nodes in our **data** graph
